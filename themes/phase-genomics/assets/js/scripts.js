f3da8f0880ce6a7be303551ded3d6eef75b2a4f6
Tu Luong <tu@delindesign.com>
2018-05-18 11:29:19 -0400

    Update .gitignore, use SCSS for styling

--- themes/phase-genomics/assets/js/scripts.js
+++ themes/phase-genomics/assets/js/scripts.js
@@ -6,8 +6,8 @@ and Foundation play nice together.
 
 jQuery(document).ready(function() {
 
-    // Remove empty P tags created by WP inside of Accordion and Orbit
-    jQuery('.accordion p:empty, .orbit p:empty').remove();
+  // Remove empty P tags created by WP inside of Accordion and Orbit
+  jQuery('.accordion p:empty, .orbit p:empty').remove();
 
 	 // Makes sure last grid item floats left
 	jQuery('.archive-grid .columns').last().addClass( 'end' );

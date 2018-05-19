7bf5801a79de3865e71e6d9c8f6802fac2316a99
Tu Luong <tu@delindesign.com>
2018-05-19 13:06:45 -0400

    Support Foundation icons, finish Header desktop

--- themes/phase-genomics/header.php
+++ themes/phase-genomics/header.php
@@ -1,6 +1,20 @@
+<?php
+	/**
+	 * Main Header
+	 *
+	 * @category   Components
+	 * @package    WordPress
+	 * @subpackage Phase Genomics
+	 * @author     Delin Design <contact@delindesign.com>
+	 * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+	 * @link       https://delindesign.com
+	 */
+
+?>
+
 <!doctype html>
 
-  <html class="no-js"  <?php language_attributes(); ?>>
+	<html class="no-js"  <?php language_attributes(); ?>>
 
 	<head>
 		<meta charset="utf-8">
@@ -15,19 +29,19 @@
 		<!-- If Site Icon isn't set in customizer -->
 		<?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) { ?>
 			<!-- Icons & Favicons -->
-			<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-			<link href="<?php echo get_template_directory_uri(); ?>/assets/images/apple-icon-touch.png" rel="apple-touch-icon" />
+			<link rel="icon" href="<?php echo esc_html( get_template_directory_uri() ); ?>/favicon.png">
+			<link href="<?php echo esc_html( get_template_directory_uri() ); ?>/assets/images/apple-icon-touch.png" rel="apple-touch-icon" />
 			<!--[if IE]>
-				<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+				<link rel="shortcut icon" href="<?php echo esc_html( get_template_directory_uri() ); ?>/favicon.ico">
 			<![endif]-->
 			<meta name="msapplication-TileColor" content="#f01d4f">
-			<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/images/win8-tile-icon.png">
-	    	<meta name="theme-color" content="#121212">
-	    <?php } ?>
+			<meta name="msapplication-TileImage" content="<?php echo esc_html( get_template_directory_uri() ); ?>/assets/images/win8-tile-icon.png">
+				<meta name="theme-color" content="#121212">
+			<?php } ?>
 		<meta property="og:image" content="https://phasegenomics.com/wp-content/uploads/2017/12/horiz-dark-sq.png" />
 		<link rel="img_src" href="https://phasegenomics.com/wp-content/uploads/2017/12/horiz-dark-sq.png" />
 
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 		<?php wp_head(); ?>
 
@@ -43,12 +57,10 @@
 			<?php get_template_part( 'parts/content', 'offcanvas' ); ?>
 
 			<div class="off-canvas-content" data-off-canvas-content>
-
 				<header class="header" role="banner">
 						<div class="container">
 							<div class="grid-x grid-margin-x">
 								<?php get_template_part( 'parts/nav', 'offcanvas-topbar' ); ?>
 							</div>
 						</div>
-
 				</header> <!-- end .header -->
--- themes/phase-genomics/parts/nav-offcanvas-topbar.php
+++ themes/phase-genomics/parts/nav-offcanvas-topbar.php
@@ -1,29 +1,41 @@
-<!-- By default, this menu will use off-canvas for small
-	 and a topbar for medium-up -->
+<?php
+	/**
+	 * Offcanvas Menu
+	 *
+	 * @category   Components
+	 * @package    WordPress
+	 * @subpackage Phase Genomics
+	 * @author     Delin Design <contact@delindesign.com>
+	 * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+	 * @link       https://delindesign.com
+	 */
+
+?>
 
 <div class="top-bar" id="top-bar-menu">
 	<div class="top-bar-left float-left">
 		<?php
-			$image = get_field('header_logo', 'option');
-			if ( !empty( $image ) ):
+		$image = get_field( 'header_logo', 'option' );
+		if ( ! empty( $image ) ) :
 		?>
-		<a href="<?php echo home_url(); ?>"><img class="header-logo" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" /></a>
+		<a href="<?php echo esc_html( home_url() ); ?>"><img class="header-logo" src="<?php echo esc_html( $image['url'] ); ?>" alt="<?php echo esc_html( $image['alt'] ); ?>" /></a>
 		<?php endif; ?>
 	</div>
 
 	<div class="top-bar-right show-for-medium">
 		<?php
-			// wp_nav_menu(array(
-			// 	'menu' => 'Social Media',
-			// 	'menu_class' => 'menu menu-social-media'
-			// ));
+			wp_nav_menu(array(
+				'container'  => false,
+				'menu'       => 'Social Media',
+				'menu_class' => 'menu menu-social-media',
+			));
 		?>
 		<?php joints_top_nav(); ?>
 	</div>
 
 	<div class="top-bar-right float-right show-for-small-only">
 		<ul class="menu">
-			<li id="menu-icon"><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
+			<li id="menu-icon"><a data-toggle="off-canvas"><?php esc_html_e( 'Menu', 'phasegenomics' ); ?></a></li>
 		</ul>
 	</div>
 

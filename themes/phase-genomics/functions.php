3b73707c552a126392b5864150f59f046c755a70
Tu Luong <tu@delindesign.com>
2018-05-26 20:46:28 -0400

    Update Montserrat font for light and bold

--- themes/phase-genomics/functions.php
+++ themes/phase-genomics/functions.php
@@ -1,65 +1,74 @@
 <?php
-// Theme support options
-require_once(get_template_directory().'/assets/functions/theme-support.php');
+	/**
+	 * Template Name: Main Theme Functions
+	 *
+	 * @category   Function
+	 * @package    WordPress
+	 * @subpackage PhaseGenomics
+	 * @author     Delin Design <contact@delindesign.com>
+	 * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+	 * @link       https://delindesign.com
+	 */
 
-// WP Head and other cleanup functions
-require_once(get_template_directory().'/assets/functions/cleanup.php');
+// Theme support options.
+require_once get_template_directory() . '/assets/functions/theme-support.php';
 
-// Register scripts and stylesheets
-require_once(get_template_directory().'/assets/functions/enqueue-scripts.php');
+// WP Head and other cleanup functions.
+require_once get_template_directory() . '/assets/functions/cleanup.php';
 
-// Register custom menus and menu walkers
-require_once(get_template_directory().'/assets/functions/menu.php');
+// Register scripts and stylesheets.
+require_once get_template_directory() . '/assets/functions/enqueue-scripts.php';
 
-// Register sidebars/widget areas
-require_once(get_template_directory().'/assets/functions/sidebar.php');
+// Register custom menus and menu walkers.
+require_once get_template_directory() . '/assets/functions/menu.php';
 
-// Makes WordPress comments suck less
-require_once(get_template_directory().'/assets/functions/comments.php');
+// Register sidebars/widget areas.
+require_once get_template_directory() . '/assets/functions/sidebar.php';
 
-// Replace 'older/newer' post links with numbered navigation
-require_once(get_template_directory().'/assets/functions/page-navi.php');
+// Makes WordPress comments suck less.
+require_once get_template_directory() . '/assets/functions/comments.php';
 
-// Adds support for multiple languages
-require_once(get_template_directory().'/assets/translation/translation.php');
+// Replace 'older/newer' post links with numbered navigation.
+require_once get_template_directory() . '/assets/functions/page-navi.php';
 
-// Remove 4.2 Emoji Support
-// require_once(get_template_directory().'/assets/functions/disable-emoji.php');
+// Adds support for multiple languages.
+require_once get_template_directory() . '/assets/translation/translation.php';
+
+// phpcs:disable
+// Remove 4.2 Emoji Support.
+// require_once get_template_directory() . '/assets/functions/disable-emoji.php';
 
 // Adds site styles to the WordPress editor
-//require_once(get_template_directory().'/assets/functions/editor-styles.php');
+//require_once get_template_directory() . '/assets/functions/editor-styles.php';
 
 // Related post function - no need to rely on plugins
-// require_once(get_template_directory().'/assets/functions/related-posts.php');
+// require_once get_template_directory() . '/assets/functions/related-posts.php';
 
 // Use this as a template for custom post types
-// require_once(get_template_directory().'/assets/functions/custom-post-type.php');
+// require_once get_template_directory() . '/assets/functions/custom-post-type.php';
 
 // Customize the WordPress login menu
-// require_once(get_template_directory().'/assets/functions/login.php');
+// require_once get_template_directory() . '/assets/functions/login.php';
 
 // Customize the WordPress admin
-// require_once(get_template_directory().'/assets/functions/admin.php');
+// require_once get_template_directory() . '/assets/functions/admin.php';
 
-// Miscellaneous function
-require_once(get_template_directory().'/assets/functions/misc.php');
+// phpcs:enable
 
-// Hide Admin Bar
-add_filter('show_admin_bar', '__return_false');
+// Miscellaneous function.
+require_once get_template_directory() . '/assets/functions/misc.php';
 
-// Option Page
-if( function_exists('acf_add_options_page') ) {
+// Option Page.
+if ( function_exists( 'acf_add_options_page' ) ) {
 	acf_add_options_page();
 }
 
-// Google Font
+/**
+ * Google Font.
+ */
 function wpb_add_google_fonts() {
-
-wp_enqueue_style( 'montserrat-sans', 'https://fonts.googleapis.com/css?family=Montserrat:400,700', false );
-
-wp_enqueue_style( 'open-sans', 'https://fonts.googleapis.com/css?family=Open+Sans:600i', false );
-
+	wp_enqueue_style( 'montserrat-sans', 'https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700"', false );
+	wp_enqueue_style( 'open-sans', 'https://fonts.googleapis.com/css?family=Open+Sans:600i', false );
 }
-
 
 add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
